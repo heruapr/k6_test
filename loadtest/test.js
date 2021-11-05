71c7d8841b9220d6c41478710f4868bf8dec115c
Heru Apriadi <heru@investree.id>
2021-11-05 13:59:08 +0700

    test

--- loadtest/test.js
+++ loadtest/test.js
@@ -2,27 +2,19 @@ import { check, sleep } from 'k6';
 import http from 'k6/http';
 
 export let options = {
-  // thresholds: {
-  //   // fail the test if 95th percentile response goes above 500ms
-  //   http_req_duration: ['p(95)<500'],
-  //   // http_req_failed: ['rate>0.01']
-  // },
-
-    stages: [
-      { duration: '1m', target: 500 },
-      { duration: '2m', target: 1000 },
-      { duration: '4m', target: 50 },
-      { duration: '2m', target: 0 },
-    ]
+  thresholds: {
+    // fail the test if avg percentile response goes above 500ms
+    http_req_duration: ['avg<500'],
+    http_req_failed: ['rate<0.25'],
+  }
   };
 
 export default function () {
-  var url = 'https://google.co.id'
-
+  var url = 'https://github.com'
  http.get(url);
- var res =  http.get(url);
- check(res, {
-        "response code was 200": (res) => res.status == 200
- })
-sleep(2)
+//  var res =  http.get(url);
+//  check(res, {
+//         "response code was 200": (res) => res.status == 200
+//  })
+ sleep(1)
 }
