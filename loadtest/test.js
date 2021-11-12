90a23aa2d3edab6a22c471ae13c6d76e37da653e
Heru Apriadi <heru@investree.id>
2021-11-12 11:22:46 +0700

    aasdas

--- loadtest/test.js
+++ loadtest/test.js
@@ -2,19 +2,23 @@ import { check, sleep } from 'k6';
 import http from 'k6/http';
 
 export let options = {
-  thresholds: {
-    // fail the test if avg percentile response goes above 500ms
-    http_req_duration: ['avg<500'],
-    http_req_failed: ['rate<0.25'],
-  }
+  vus: 10000,
+  duration: '5s',
+  iterations : 10000
+  // thresholds: {
+  //   // fail the test if avg percentile response goes above 500ms
+  //   http_req_duration: ['avg<500'],
+  //   http_req_failed: ['rate<0.25'],
+  // }
   };
 
 export default function () {
-  var url = 'https://github.com'
- http.get(url);
-//  var res =  http.get(url);
-//  check(res, {
-//         "response code was 200": (res) => res.status == 200
-//  })
- sleep(1)
+  var url = 'https://blazedemo.com/'
+//  http.get(url);
+ var res =  http.get(url);
+ check(res, {
+        "response code was 200": (res) => res.status == 200
+ })
+ console.log(res.status)
+ sleep(2)
 }
